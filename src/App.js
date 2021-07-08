b0a343b9cb0cc55e7c9a64602431814e18cfc5cd
Sohyeon.P <hidhen9714@naver.com>
2021-07-08 17:00:00 +0900

    lastcommit from company

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import Counter from './Counter';
 import Say from './Say';
+import EventPractice from './EventPractice';
 const App = () => {
-  return <Say />;
+  return <EventPractice />;
 };
 
 export default App;
